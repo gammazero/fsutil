1e20dd1019d1039e7f582c398c3854db20dd9c9b
Andrew Gillis <gammazero@users.noreply.github.com>
2022-11-09 14:58:10 -0800

    Initial commit

---
+++ fsutil.go
@@ -0,0 +1,80 @@
+package fsutil
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"time"
+)
+
+// DirExists checks if a directory exists.
+func DirExists(dir string) (bool, error) {
+	if dir == "" {
+		return false, errors.New("directory not specified")
+	}
+
+	fi, err := os.Stat(dir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
+	}
+	if !fi.IsDir() {
+		return false, fmt.Errorf("not a directory: %s", dir)
+	}
+	return true, nil
+}
+
+// DirWritable checks if a directory is writable. If the directory does
+// not exist it is created with writable permission.
+func DirWritable(dir string) error {
+	if dir == "" {
+		return errors.New("directory not specified")
+	}
+
+	if _, err := os.Stat(dir); err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			// Directory does not exist, so create it.
+			err = os.Mkdir(dir, 0775)
+			if err == nil {
+				return nil
+			}
+		}
+		if errors.Is(err, fs.ErrPermission) {
+			err = fs.ErrPermission
+		}
+		return fmt.Errorf("directory not writable: %s: %w", dir, err)
+	}
+
+	// Directory exists, check that a file can be written.
+	file, err := os.CreateTemp(dir, "writetest")
+	if err != nil {
+		if errors.Is(err, fs.ErrPermission) {
+			err = fs.ErrPermission
+		}
+		return fmt.Errorf("directory not writable: %s: %w", dir, err)
+	}
+	file.Close()
+	return os.Remove(file.Name())
+}
+
+// FileChanged returns the modification time of a file and true if different
+// from the given time.
+func FileChanged(filePath string, modTime time.Time) (time.Time, bool, error) {
+	fi, err := os.Stat(filePath)
+	if err != nil {
+		return modTime, false, fmt.Errorf("cannot stat file %s: %w", filePath, err)
+	}
+	if fi.ModTime() != modTime {
+		return fi.ModTime(), true, nil
+	}
+	return modTime, false, nil
+}
+
+// FileExists returns true if the file exists.
+func FileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !errors.Is(err, fs.ErrNotExist)
+}
---
+++ fsutil_test.go
@@ -0,0 +1,104 @@
+package fsutil_test
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/gammazero/fsutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDirExists(t *testing.T) {
+	_, err := fsutil.DirExists("")
+	require.Error(t, err)
+
+	tmpDir := t.TempDir()
+	exists, err := fsutil.DirExists(tmpDir)
+	require.NoError(t, err)
+	require.True(t, exists)
+
+	notDir := filepath.Join(tmpDir, "nosuchdir")
+	exists, err = fsutil.DirExists(notDir)
+	require.NoError(t, err)
+	require.False(t, exists)
+
+	file, err := os.CreateTemp(t.TempDir(), "")
+	require.NoError(t, err)
+	require.NoError(t, file.Close())
+
+	_, err = fsutil.DirExists(file.Name())
+	require.ErrorContains(t, err, "not a directory")
+
+	require.NoError(t, os.Chmod(tmpDir, 0222))
+	_, err = fsutil.DirExists(notDir)
+	require.Error(t, err)
+
+	require.NoError(t, os.Chmod(tmpDir, 0777))
+}
+
+func TestDirWritable(t *testing.T) {
+	err := fsutil.DirWritable("")
+	require.Error(t, err)
+
+	tmpDir := t.TempDir()
+
+	wrDir := filepath.Join(tmpDir, "readwrite")
+	err = fsutil.DirWritable(wrDir)
+	require.NoError(t, err)
+
+	// Check that DirWritable created directory.
+	fi, err := os.Stat(wrDir)
+	require.NoError(t, err)
+	require.True(t, fi.IsDir())
+
+	err = fsutil.DirWritable(wrDir)
+	require.NoError(t, err)
+
+	roDir := filepath.Join(tmpDir, "readonly")
+	if err = os.Mkdir(roDir, 0500); err != nil {
+		panic(err)
+	}
+
+	err = fsutil.DirWritable(roDir)
+	require.ErrorIs(t, err, fs.ErrPermission)
+
+	roChild := filepath.Join(roDir, "child")
+	err = fsutil.DirWritable(roChild)
+	require.ErrorIs(t, err, fs.ErrPermission)
+}
+
+func TestFileChanged(t *testing.T) {
+	file, err := os.CreateTemp(t.TempDir(), "")
+	require.NoError(t, err)
+	require.NoError(t, file.Close())
+
+	var modTime time.Time
+	var changed bool
+	modTime, changed, err = fsutil.FileChanged(file.Name(), modTime)
+	require.NoError(t, err)
+	require.True(t, changed)
+	require.False(t, modTime.IsZero())
+
+	var newModTime time.Time
+	newModTime, changed, err = fsutil.FileChanged(file.Name(), modTime)
+	require.NoError(t, err)
+	require.False(t, changed)
+	require.Equal(t, modTime, newModTime)
+
+	_, _, err = fsutil.FileChanged(filepath.Join(t.TempDir(), "nosuchfile"), modTime)
+	require.Error(t, err)
+}
+
+func TestFileExists(t *testing.T) {
+	fileName := filepath.Join(t.TempDir(), "somefile")
+	require.False(t, fsutil.FileExists(fileName))
+
+	file, err := os.Create(fileName)
+	require.NoError(t, err)
+	file.Close()
+
+	require.True(t, fsutil.FileExists(fileName))
+}
