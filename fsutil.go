8537d8baebf9f0910926aeea29f984e8c7ea965a
Andrew Gillis <11790789+gammazero@users.noreply.github.com>
2024-09-23 17:00:50 -0700

    Expand function expands "~" at the start of a path (#8)
    * Use os.UserHomeDir to expand "~"
    * Update dependencies

--- fsutil.go
+++ fsutil.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/fs"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -98,4 +99,28 @@ func FileChanged(filePath string, modTime time.Time) (time.Time, bool, error) {
 func FileExists(filename string) bool {
 	_, err := os.Stat(filename)
 	return !errors.Is(err, fs.ErrNotExist)
+}
+
+// Expand expands the path to include the home directory if the path is
+// prefixed with `~`. If it isn't prefixed with `~`, the path is returned
+// as-is.
+func Expand(path string) (string, error) {
+	if path == "" {
+		return path, nil
+	}
+
+	if path[0] != '~' {
+		return path, nil
+	}
+
+	if len(path) > 1 && path[1] != '/' && path[1] != '\\' {
+		return "", errors.New("cannot expand user-specific home dir")
+	}
+
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(dir, path[1:]), nil
 }
