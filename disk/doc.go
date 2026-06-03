24dab9bd6abc05b96a3001ce12c75ec0462bac04
Andrew Gillis <11790789+gammazero@users.noreply.github.com>
2026-06-02 14:14:54 -1000

    add doc.go for disk package (#12)

---
+++ disk/doc.go
@@ -0,0 +1,4 @@
+// Package disk provides functionality for reporting disk usage on multiple platforms.
+//
+// Supported platforms: aix, darwin (macos), freebsd, linux, openbsd, windows
+package disk
